e7353e6267148f070fe3bab6e34bd2b981f3bbd6
benfattori <ben.fattori@mail.utoronto.ca>
2020-11-01 18:14:48 -0500

    Added 3-variable test

--- autodiff_tests.py
+++ autodiff_tests.py
@@ -52,6 +52,30 @@ def function_test_2var(func, values, expected_outputs):
         print('Error, check computed gradients')
 
 
+"""
+Three variable function tests
+"""
+
+
+def function_test_3var(func, values, expected_outputs):
+    # Init computational graph
+    graph = G.Computational_Graph()
+    values_array = []
+    for i in values:
+        k = Node(i, 'Leaf', graph)
+        values_array.append(k)
+        graph(k)
+
+    out = func(values_array[0], values_array[1], values_array[2]).value
+    grad = graph.backward()
+    if out == expected_outputs[0] and grad == expected_outputs[1]:
+        pass
+    else:
+        print(out[0])
+        print(grad)
+        print('Error, check computed gradients')
+
+
 if __name__ == "__main__":
 
     def a(x):
@@ -72,26 +96,38 @@ if __name__ == "__main__":
     function_test_1var(
         d, [1], [0.5143952585235492, [-2.1648184471903296]])
 
-    def a(x1, x2):
+    def e(x1, x2):
         return ops.ln(x1) + x1*x2 - ops.sin(x2)
 
-    def b(x1, x2):
+    def f(x1, x2):
         return x1*x2 - ops.exp(x1-x2)*ops.sin(x1)
 
-    def c(x1, x2):
+    def g(x1, x2):
         return ops.exp(x1)*ops.sin(x2) + ops.exp(x2)*ops.cos(x1)
 
-    def d(x1, x2):
+    def h(x1, x2):
         return x1*(ops.exp(-(x1**2 + x2**2)))
 
     function_test_2var(
-        a, [2, 5], [11.652071455223084, [5.5, 1.7163378145367738]])
+        e, [2, 5], [11.652071455223084, [5.5, 1.7163378145367738]])
 
-    function_test_2var(b, [3, 2], [5.616396046458869, [
+    function_test_2var(f, [3, 2], [5.616396046458869, [
         4.307474660278663, 3.383603953541131]])
 
-    function_test_2var(c, [6, -2], [-366.70681891263416, [
+    function_test_2var(g, [6, -2], [-366.70681891263416, [
         -366.79894905474396, -167.75567126633288]])
 
-    function_test_2var(d, [-1, 3], [-4.5399929762484854e-05, [
+    function_test_2var(h, [-1, 3], [-4.5399929762484854e-05, [
         -4.5399929762484854e-05, 0.0002723995785749091]])
+
+    def k(x1, x2, x3):
+        return x1**2 + x2**2 + x3**2
+
+    function_test_3var(
+        k, [1, 1, 1], [3, [2, 2, 2]])
+
+    # def l(x1, x2, x3)
+
+    # def m(x1, x2, x3)
+
+    # def n(x1, x2, x3)
