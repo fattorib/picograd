e93d627d66d56126ff31376946e9977b1c7a8cc4
benfattori <ben.fattori@mail.utoronto.ca>
2020-11-01 18:04:47 -0500

    Implemented single variable tests

--- autodiff_tests.py
+++ autodiff_tests.py
@@ -9,6 +9,25 @@ Single Variable Function Tests
 """
 
 
+def function_test_1var(func, value, expected_output):
+    # Init computational graph
+    graph = G.Computational_Graph()
+    values_array = []
+    for i in value:
+        k = Node(i, 'Leaf', graph)
+        values_array.append(k)
+        graph(k)
+
+    out = func(values_array[0]).value
+    grad = graph.backward()
+    if out == expected_output[0] and grad == expected_output[1]:
+        print('Test passed')
+    else:
+        print(out)
+        print(grad)
+        print('Error, check computed gradients')
+
+
 """
 Two Variable Function Tests
 """
@@ -28,12 +47,31 @@ def function_test_2var(func, values, expected_outputs):
     if out == expected_outputs[0] and grad == expected_outputs[1]:
         print('Test passed')
     else:
-        print(out)
+        print(out[0])
         print(grad)
         print('Error, check computed gradients')
 
 
 if __name__ == "__main__":
+
+    def a(x):
+        return (ops.exp(x)+1).recip()
+
+    def b(x):
+        return (ops.exp(x*2) - 1)/(ops.exp(x*2) + 1)
+
+    def c(x):
+        return ops.ln(x) + ops.exp(x**2)
+
+    def d(x):
+        return ops.sin(ops.cos(ops.ln(x) + x**2))
+
+    print('Single Variable Tests:')
+    function_test_1var(a, [0], [0.5, [-0.25]])
+    function_test_1var(b, [1], [0.7615941559557649, [0.419974341614026]])
+    function_test_1var(c, [np.pi], [19334.833804250986, [121477.46943551568]])
+    function_test_1var(
+        d, [1], [0.5143952585235492, [-2.1648184471903296]])
 
     def a(x1, x2):
         return ops.ln(x1) + x1*x2 - ops.sin(x2)
@@ -47,6 +85,8 @@ if __name__ == "__main__":
     def d(x1, x2):
         return x1*(ops.exp(-(x1**2 + x2**2)))
 
+    print()
+    print('Two Variable Tests:')
     function_test_2var(
         a, [2, 5], [11.652071455223084, [5.5, 1.7163378145367738]])
 
