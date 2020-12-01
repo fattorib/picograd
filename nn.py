c8453a5b8f28315482e800f6d5f35fad52090a25
benfattori <ben.fattori@mail.utoronto.ca>
2020-12-01 11:40:38 -0500

    Linear layer

---
+++ nn.py
@@ -0,0 +1,24 @@
+import numpy as np
+from Tensor import Tensor
+
+
+class Linear():
+
+    def __init__(self, in_feats, out_feats, bias=True):
+        self.in_feats = in_feats
+        self.out_feats = out_feats
+        self.weights = Tensor.random_uniform(self.in_feats, self.out_feats)
+        self.bias_flag = bias
+
+        if self.bias_flag:
+            self.bias = Tensor.random_uniform(1, self.out_feats)
+
+    def forward(self, input):
+        return input.dot(self.weights)+self.bias
+
+    def __call__(self, input):
+        return input.dot(self.weights)+self.bias
+
+
+if __name__ == "__main__":
+    print('Hey')
