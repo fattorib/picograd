exp overflow errors need to be fixed...

Loss.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
 
     print(x.shape)
 
-    mse = NLLLoss()
+    mse = MSELoss()
 
     z = mse(x, y)
     print(z)
@@ -51,7 +51,7 @@ if __name__ == "__main__":
     y = torch.tensor([1.0, 0.0, 0.0], requires_grad=True).type(torch.long)
     print(y.shape)
 
-    mse = torch.nn.NLLLoss()
+    mse = torch.nn.MSELoss()
     z = mse(x, y)
     print(z)
     z.backward()

Network.py
@@ -1,8 +1,13 @@
+import matplotlib.pyplot as plt
 from Tensor import *
 
-from nn import Linear, ReLU, LogSoftmax
+from nn import Linear, ReLU, LogSoftmax, Sigmoid
 
 from Loss import NLLLoss
+
+from optim import SGD
+
+from GetMNIST import fetch_mnist
 
 
 class Network():
@@ -12,17 +17,46 @@ class Network():
         self.fc2 = Linear(128, 10)
         self.logsoftmax = LogSoftmax()
         self.relu = ReLU()
+        self.sigmoid = Sigmoid()
 
     def forward(self, input):
-        x = self.relu(self.fc1(input))
+        x = self.sigmoid(self.fc1(input))
         x = self.fc2(x)
         return self.logsoftmax(x, 1)
 
+    def parameters(self):
+        return [self.fc1.weights, self.fc2.weights, self.fc1.bias, self.fc2.bias]
+
 
 model = Network()
+optimizer = SGD(model.parameters(), lr=0.1)
+criterion = NLLLoss()
 
-x_init = np.random.randn(2, 784).astype(np.float32)
+# Batching code
+X_train, Y_train, X_test, Y_test = fetch_mnist()
 
-single_batch = Tensor(x_init)
+epochs = 1
+losses = []
 
-print(model.forward(single_batch))
+for i in range(0, epochs):
+    samp = np.random.randint(0, X_train.shape[0], size=(64))
+
+    x = Tensor(X_train[samp].reshape((-1, 28*28)).astype(np.float32))
+    Y = Y_train[samp]
+    y = np.zeros((len(samp), 10), np.float32)
+    # correct loss for NLL, torch NLL loss returns one per row
+    y[range(y.shape[0]), Y] = 1
+    y = Tensor(y)
+
+    optimizer.zero_grad()
+
+    out = model.forward(x)
+    # print(out)
+    loss = criterion(out, y)
+    loss.backward()
+    losses.append(loss.value)
+    optimizer.step()
+    # print(loss)
+
+# plt.plot(range(0, epochs), losses)
+# plt.show()

Tensor.py
@@ -45,6 +45,9 @@ class Tensor():
 
         self.grad = np.zeros_like(self.value, dtype=np.float32)
 
+    def zero_grad(self):
+        self.grad = np.zeros_like(self.value, dtype=np.float32)
+
     def expand_dim(self, axis):
         self.value = np.expand_dims(self.value, axis)
         self.shape = self.value.shape
@@ -60,8 +63,9 @@ class Tensor():
                         children=(self, other), fun='AddBackward')
 
         def _backward():
-            self.grad += output.grad
-            other.grad += output.grad
+            # Broadcasting is unhappy with +=
+            self.grad = self.grad + output.grad
+            other.grad = other.grad + output.grad
 
         output._backward = _backward
 

nn.py
@@ -42,7 +42,7 @@ class Sigmoid():
     @staticmethod
     def __call__(input):
 
-        val = 1/(1+np.exp(-(input.value)))
+        val = 1./(1.+np.exp(-(input.value)))
 
         output = Tensor(val,
                         children=(input,), fun='SigmoidBackard')