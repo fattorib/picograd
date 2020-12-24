11401a6e7f2a9b7dec9504d5f29422719fb88c31
benfattori <ben.fattori@mail.utoronto.ca>
2020-12-24 13:49:39 -0500

    Adding tests

---
+++ test/tensor_tests.py
@@ -0,0 +1,11 @@
+import torch
+import numpy as np
+import unittest
+from MiniNN.Tensor import Tensor
+
+
+class TestTensorBasics(unittest.TestCase):
+
+
+if __name__ == '__main__':
+    unittest.main()
